c1f6d68fe8be42dc163f2babbbffc71ef8774b69
kiongosss <user@example.com>
2025-06-02 15:23:18 +0300

    Add Next.js config to ignore ESLint errors during build

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/*
+Project Name: Flexit
+Project URI: 
+Description: A web app where users pay to flex on a public leaderboard
+Version: 1.0.0
+Author: KaseeMoka
+Author URI: https://github.com/kiongosss
+Text Domain: 
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
